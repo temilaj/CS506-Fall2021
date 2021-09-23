e78dc8aaff3882d29b9d285a6ee1be1218ac6d05
temilaj <temilaj@hotmail.com>
2021-09-23 12:00:00 -0400

    add road drawing

--- 01-python/city.py
+++ 01-python/city.py
@@ -5,16 +5,16 @@ from education.education import draw_education
 from infrastructure import road, power, tree
 
 def draw_city():
-    draw_safety()
+    # draw_safety()
     road.draw_road()
-    power.draw_power_plant()
-    road.draw_road()
-    draw_leisure()
-    road.draw_road()
-    draw_outdoors()
-    tree.draw_tree()
-    road.draw_road()
-    draw_education()
+    # power.draw_power_plant()
+    # road.draw_road()
+    # draw_leisure()
+    # road.draw_road()
+    # draw_outdoors()
+    # tree.draw_tree()
+    # road.draw_road()
+    # draw_education()
     return
 
 draw_city()
