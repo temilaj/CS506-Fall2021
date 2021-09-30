ee6440295cff1590f8bce4205eeadb0e285a308a
temilaj <temilaj@hotmail.com>
2021-09-30 12:16:41 -0400

    complete sim.py

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,17 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    with open(csv_file_path) as f:
+        lines = f.readlines()
+        parsed_file = []
+        for line in lines:
+            line = line.replace('\n', '').split(',')
+            for index in range(len(line)):
+                if line[index].isdigit():
+                    line[index] = int(line[index])
+                else:
+                    line[index] = line[index].strip('\"')
+            parsed_file.append(line)
+
+        return parsed_file;
+        
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -5,12 +5,52 @@ def euclidean_dist(x, y):
     return res**(1/2)
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    result = 0
+
+    for i in range(len(x)):
+        result += abs(x[i] - y[i])
+    return result
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection_count = 0
+    for i in range(len(x)):
+        if x[i] == y[i]:
+            intersection_count += 1
+
+    union_count = len(x) + len(y) - intersection_count
+    
+    result = 0
+    try:
+        result = intersection_count/union_count
+        return result
+    except:
+        return result
+
+
+def transpose(matrix):
+    return list(map(list, zip(*matrix)))
+
+def norm(x):
+    result = 0
+    for i in range(len(x)):
+        result += x[i]**2
+    return result**(1/2)
+
+def multiply(x,y):
+    result = 0
+    for i in range(len(x)):
+        result += x[i]* y[i]
+    return result
+
+def cosine(x,y):
+    result = 0
+    try:
+        result = multiply(transpose(x), y) / (norm(x) *norm(y))
+        return result
+    except:
+        return result
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    return 1 - cosine(x,y)
 
 # Feel free to add more
